9196bafb816e93bdda3436c2fad1ecf9d3bad981
jadefalco <jadefalco@gmail.com>
2026-07-02 15:24:28 -0700

    GSC

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
